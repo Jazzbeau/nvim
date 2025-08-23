0906dc0273a00e3f99bbe8f532d4f30eb6bc874b
jazzbeau <jazzbeaumcinnes@gmail.com>
2025-08-23 12:14:59 +0930

    Update config
    Update nvim config to settings as of 23.08.25

--- init.lua
+++ init.lua
@@ -1,31 +1,32 @@
-require 'core.options'
-require 'core.keymaps'
+require("core.options")
+require("core.keymaps")
 
 -- Store path to lazyvim
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 -- If not installed, fetch from repo
 if not vim.uv.fs_stat(lazypath) then
-    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
     -- Handle error
     if vim.v.shell_error ~= 0 then
-        error('Error cloning lazy.nvim:\n' .. out)
+        error("Error cloning lazy.nvim:\n" .. out)
     end
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 -- Install plugins via LazyVim
-require('lazy').setup({
+require("lazy").setup({
     -- NeoTree; file-manager
-    require 'plugins.theme',
-    require 'plugins.neotree',
-    require 'plugins.bufferline',
-    require 'plugins.lualine',
-    require 'plugins.treesitter',
-    require 'plugins.telescope',
-    require 'plugins.lsp',
-    require 'plugins.autocomplete',
-    require 'plugins.autoformat',
+    require("plugins.theme"),
+    require("plugins.neotree"),
+    require("plugins.bufferline"),
+    require("plugins.lualine"),
+    require("plugins.treesitter"),
+    require("plugins.telescope"),
+    require("plugins.lsp"),
+    require("plugins.autocomplete"),
+    require("plugins.autoformat"),
+    require("plugins.indent-blankline"),
+    -- require("plugins.delimitMate"),
 })
-
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -5,15 +5,14 @@ vim.wo.number = true
 vim.o.relativenumber = true
 
 -- Sync clipboards with system
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 -- Remove line wrapping
 vim.o.wrap = false
 vim.o.linebreak = true
 
 -- Enable mouse mode, for when necessary
-vim.o.mouse = 'a'
-
+vim.o.mouse = "a"
 
 -- Copy indent from current line when starting a new one
 vim.o.autoindent = true
@@ -32,4 +31,14 @@ vim.o.softtabstop = 4
 -- Convert tabs to spaces
 vim.o.expandtab = true
 
-vim.opt.formatoptions:remove { "c", "r", "o" } 
+vim.diagnostic.config({ virtual_text = true })
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
+})
