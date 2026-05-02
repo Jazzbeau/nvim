8256d5d9c3217b8b339118b2638d3bfb3f541cfe
jazzbeau <jazz@jazzbe.au>
2026-05-03 00:12:10 +0930

    Update nvim dotfiles

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -5,11 +5,24 @@ return {
 			lua = { "stylua" },
 			python = { "isort", "black" },
 			rust = { "rustfmt", lsp_format = "fallback" },
+			csharp = { "csharpier", stop_after_first = true },
 			-- JS/TS
 			typescript = { "prettierd", "prettier", stop_after_first = true },
 			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 			javascript = { "prettierd", "prettier", stop_after_first = true },
 			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+		},
+		formatters = {
+			csharpier = {
+				-- Update as per CSharpier 1.0.1+ changes
+				command = "csharpier",
+				args = { "format", "--write-stdout" },
+				-- "two_way" is used in some configs to handle stdin/stdout correctly
+			},
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
 		},
 	},
 	config = function(_, opts)
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,12 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	"olimorris/onedarkpro.nvim",
+	priority = 1000, -- Ensure it loads first
+	-- "catppuccin/nvim",
+	-- name = "catppuccin",
 	lazy = false,
 	priority = 1000,
 	config = true,
 	opts = function()
-		vim.cmd("colorscheme catppuccin")
+		vim.cmd("colorscheme onedark_dark")
 	end,
 }
